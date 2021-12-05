0d543b927a0e3c98323cc8233d1ba09c090c6a22
squid233 <513508220@qq.com>
2021-12-05 21:54:11 +0800

    Waiting for GLUtils 1.6.0

--- src/main/java/org/overrun/real2d/client/Main.java
+++ src/main/java/org/overrun/real2d/client/Main.java
@@ -1,10 +1,19 @@
 package org.overrun.real2d.client;
 
+import org.overrun.glutils.game.GameApp;
+import org.overrun.glutils.game.GameConfig;
+
 /**
  * @author squid233
  * @since 0.1.0
  */
 public class Main {
     public static void main(String[] args) {
+        GameConfig config = new GameConfig();
+        config.width = 854;
+        config.height = 480;
+        config.title = "Real2D " + Real2D.VERSION;
+        new GameApp(Real2D.instance = new Real2D(),
+            config);
     }
 }
--- src/main/java/org/overrun/real2d/client/Real2D.java
+++ src/main/java/org/overrun/real2d/client/Real2D.java
@@ -1,8 +1,47 @@
 package org.overrun.real2d.client;
 
+import org.jetbrains.annotations.Contract;
+import org.overrun.glutils.AtlasLoom;
+import org.overrun.glutils.AtlasLoomAWT;
+import org.overrun.glutils.game.Game;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.overrun.glutils.game.GLStateManager.enableTexture2D;
+
 /**
  * @author squid233
  * @since 0.1.0
  */
-public class Real2D {
+public class Real2D extends Game {
+    protected static Real2D instance;
+    public static final String VERSION = "0.1.0";
+    private AtlasLoomAWT blockAtlas;
+
+    /**
+     * Get the instance of the client.
+     *
+     * @return {@link #instance}
+     */
+    @Contract(pure = true)
+    public static Real2D getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void create() {
+        enableTexture2D();
+        blockAtlas = AtlasLoom.awt("block");
+    }
+
+    @Override
+    public void render() {
+        glClear(GL_COLOR_BUFFER_BIT);
+        super.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        glViewport(0, 0, width, height);
+        super.resize(width, height);
+    }
 }
