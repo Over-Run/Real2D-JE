1dc98b6f432350ff4053b0ec1d16a4be6a1eeae8
squid233 <513508220@qq.com>
2021-12-05 17:05:40 +0800

    Merging

--- src/main/java/org/overrun/real2d/client/Main.java
+++ src/main/java/org/overrun/real2d/client/Main.java
@@ -1,49 +1,11 @@
 package org.overrun.real2d.client;
 
-import static org.lwjgl.glfw.GLFW.glfwGetTime;
-
 /**
  * @author squid233
  * @since 0.1.0
  */
-public class Main implements Runnable, AutoCloseable {
-    private static final Real2D client = Real2D.INSTANCE;
-
-    public void start() {
-        client.init();
-        run();
-    }
-
-    @Override
-    public void run() {
-        long lastTime = (long) (glfwGetTime() * 1000);
-        int frames = 0;
-        while (!client.window.shouldClose()) {
-            client.timer.advanceTime();
-            for (int i = 0; i < client.timer.ticks; ++i) {
-                client.tick();
-            }
-            client.render();
-            client.update();
-            ++frames;
-            while (glfwGetTime() * 1000 >= lastTime + 1000L){
-                client.window.setTitle(
-                        client.appendTitle(
-                                String.format(" FPS: %d", frames)));
-                lastTime += 1000L;
-                frames = 0;
-            }
-        }
-    }
-
-    @Override
-    public void close() {
-        client.close();
-    }
+public class Main {
 
     public static void main(String[] args) {
-        try (Main main = new Main()) {
-            main.start();
-        }
     }
 }
--- src/main/java/org/overrun/real2d/client/world/entity/ClientPlayer.java
+++
@@ -1,8 +0,0 @@
-package org.overrun.real2d.client.world.entity;
-
-/**
- * @author squid233
- * @since 0.1.0
- */
-public class ClientPlayer {
-}
